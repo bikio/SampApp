49c0fc20142ca1b23b244d7102197097572577be
ageobab <agbabiki@gmail.com>
2014-07-30 15:37:28 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Barkevius Beacon"
+    email    "barkbark@example.com"
+    password "suckit"
+    password_confirmation "suckit"
+  end
+end
